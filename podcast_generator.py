61ad8564fb6dc948daa813750255b95f9b41c897
velocityspanish <velocityspanish@gmail.com>
2026-08-01 19:34:33 +0530

    fix: remove language leftovers + fonts + text layout

--- podcast_generator.py
+++ podcast_generator.py
@@ -507,12 +507,12 @@ def create_frame(turn, output_path, frame_num=0):
 
     draw.text((pill_x + pill_w + 25, pill_y + 26), "hablando", fill=LIGHT_GRAY, font=f_hablando, anchor="lm")
 
-    # === MAIN SPANISH TEXT (auto-size to fit max 3 lines) ===
-    spanish_text = turn.get("spanish", "")
-    # Calculate lines at different font sizes, pick best fit
-    chosen_font = f_spanish
+    # === MAIN TEXT (auto-size, HARD max 3 lines) ===
+    spanish_text = turn.get("spanish", turn.get("spanish", ""))
+    chosen_font = None
     chosen_lh = 90
-    for test_size in [64, 56, 48, 40, 34]:
+    final_lines = []
+    for test_size in [64, 56, 48, 40, 34, 28, 24, 20]:
         test_font = load_font(test_size, bold=True)
         test_lh = int(test_size * 1.4)
         text_words = spanish_text.split()
@@ -527,10 +527,32 @@ def create_frame(turn, output_path, frame_num=0):
                 tmp_lines.append(' '.join(cur))
                 cur = [w]
         if cur: tmp_lines.append(' '.join(cur))
-        if len(tmp_lines) <= 3 or test_size <= 34:
+        if len(tmp_lines) <= 3:
             chosen_font = test_font
             chosen_lh = test_lh
+            final_lines = tmp_lines
             break
+    if chosen_font is None:
+        chosen_font = load_font(20, bold=True)
+        chosen_lh = int(20 * 1.4)
+        text_words = spanish_text.split()
+        tmp_lines = []
+        cur = []
+        for w in text_words:
+            test = ' '.join(cur + [w])
+            bb = draw.textbbox((0, 0), test, font=chosen_font)
+            if bb[2] - bb[0] <= 1550 or not cur:
+                cur.append(w)
+            else:
+                tmp_lines.append(' '.join(cur))
+                cur = [w]
+        if cur: tmp_lines.append(' '.join(cur))
+        if len(tmp_lines) > 3:
+            tmp_lines = tmp_lines[:3]
+            if spanish_text:
+                tmp_lines[-1] = tmp_lines[-1].rstrip() + "..."
+        final_lines = tmp_lines
+        spanish_text = " ".join(final_lines)
     draw_rich_text_centered(draw, spanish_text, center_y=440, font=chosen_font, max_w=1550, line_height=chosen_lh)
 
     # === CENTER DIVIDER WITH DOT ===
